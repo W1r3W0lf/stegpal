742177d7b5c58d524bd4f5772dafc5a40bbd8ba6
W1r3W0lf <apple.link.level@gmail.com>
2019-04-04 00:01:02 -0600

    added Pixel struct and function

--- BMP/Pixels.c
+++ BMP/Pixels.c
@@ -3,3 +3,67 @@
 //
 
 #include "Pixels.h"
+
+#include <stdlib.h>
+
+Pixels constructPixels(){
+    Pixels pixels;
+    pixels.width = 0;
+    pixels.height = 0;
+    pixels.size = 0;
+    pixels.bits = 0;
+    pixels.pixels = NULL;
+    return pixels;
+}
+
+void destructPixels(Pixels pixels){
+    pixels.width = 0;
+    pixels.height = 0;
+    pixels.size = 0;
+    pixels.bits = 0;
+    free(pixels.pixels);
+}
+
+
+Pixels getPixels(DIBHeader dibHeader ,FILE *fileIn){
+    Pixels pixels = constructPixels();
+
+    switch (dibHeader.type){
+        case core:
+            pixels.bits = dibHeader.core->bcBitCount;
+            pixels.height = dibHeader.core->bcHeight;
+            pixels.width = dibHeader.core->bcWidth;
+            break;
+        case info:
+            pixels.bits = dibHeader.info->biBitCount;
+            pixels.height = dibHeader.info->biHeight;
+            pixels.width = dibHeader.info->biWidth;
+            break;
+        case v4:
+            pixels.bits = dibHeader.v4->bV4BitCount;
+            pixels.height = dibHeader.v4->bV4Height;
+            pixels.width = dibHeader.v4->bV4Height;
+            break;
+        case v5:
+            pixels.bits = dibHeader.v5->bV5BitCount;
+            pixels.height = dibHeader.v5->bV5Height;
+            pixels.width = dibHeader.v5->bV5Height;
+            break;
+    }
+
+    pixels.size = (int)( 1 + ((pixels.height * pixels.width - 1) / (pixels.bits/8.0)));
+
+    pixels.pixels = malloc(pixels.size);
+
+    if (fread(pixels.pixels, pixels.size, 1, fileIn)){
+        fprintf(stderr, "ERROR reading Pixels h%i w%i s%i b%i \n", pixels.height, pixels.width, pixels.size, pixels.bits);
+        exit(1);
+    }
+
+    return pixels;
+}
+
+
+void putPixels(Pixels pixels ,FILE *output){
+
+}
--- BMP/Pixels.h
+++ BMP/Pixels.h
@@ -6,6 +6,7 @@
 #define STEGPAL_PIXELS_H
 
 #include "../wordDefine.h"
+#include "DIBHeader.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,5 +16,22 @@ typedef struct tagRGBTRIPLE {
     BYTE rgbtGreen;
     BYTE rgbtRed;
 } RGBTRIPLE;
+
+typedef struct Pixels{
+    int width;
+    int height;
+    int size;   //Number of bytes
+    int bits;   //Number of bits per pixel
+    RGBTRIPLE **pixels;
+} Pixels;
+
+
+Pixels constructPixels();
+
+void destructPixels(Pixels pixels);
+
+Pixels getPixels(DIBHeader dibHeader ,FILE *fileIn);
+
+void putPixels(Pixels pixels ,FILE *output);
 
 #endif //STEGPAL_PIXELS_H
