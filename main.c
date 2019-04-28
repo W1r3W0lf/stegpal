9f8b7c15cdfa585f75bae078a2f6b9a4cbec309a
W1r3W0lf <apple.link.level@gmail.com>
2019-04-28 13:07:15 -0600

    packaged a bit better then all of the debug stuff that was in there

--- main.c
+++ main.c
@@ -3,23 +3,31 @@
 #include "BMP/Bitmap.h"
 #include "StegPal/stegpal.h"
 
+void printSize(char* fileName){
+    BMP freeBytes = getBMP(fileName);
+    printf("%i free bytes\n", getSpaceInBMP(freeBytes));
+    destructBMP(freeBytes);
+}
+
+void writeToBMP(char* inputFile, char* outputFile, char* message){
+    BMP bmpData = getBMP(inputFile);
+    writeMessageToBMP(bmpData,message);
+    putBMP(bmpData, outputFile);
+    destructBMP(bmpData);
+}
+
 int main(int argc, char** argv) {
 
-    if(argc < 3){
-        fprintf(stderr, "ERROR not enough inputs\n");
-        exit(1);
+    switch(argc){
+        case 2:
+            printSize(argv[1]);
+            break;
+        case 4:
+            writeToBMP(argv[1],argv[2],argv[3]);
+            break;
+        default:
+            fprintf(stderr, "ERROR wrong number of inputs\n");
+            exit(1);
     }
-
-    BMP test = getBMP(argv[1]);
-
-    printf("%i free bytes\n", getSpaceInBMP(test));
-    readHexFromBMP(test);
-    writeMessageToBMP(test,"best tests");
-    readHexFromBMP(test);
-    readMessageFromBMP(test);
-
-    putBMP(test, argv[2]);
-    destructBMP(test);
-
     return 0;
 }
