0a6c82d4b47558493c2be4df6aed9b46eb45fb92
W1r3W0lf <apple.link.level@gmail.com>
2019-09-05 20:43:13 -0600

    Added help Bumped down the version of cmake needed and added a .gitignore

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 void printSize(char* fileName){
     BMP freeBytes = getBMP(fileName);
     printf("%i free bytes\n", getSpaceInBMP(freeBytes));
+    readMessageFromBMP(freeBytes);
     destructBMP(freeBytes);
 }
 
@@ -14,6 +15,12 @@ void writeToBMP(char* inputFile, char* outputFile, char* message){
     writeMessageToBMP(bmpData,message);
     putBMP(bmpData, outputFile);
     destructBMP(bmpData);
+}
+
+void help(){
+    printf("Usage:"
+           "\nstegpal file.bmp                        prints out the free space in file.bmp and any massages."
+           "\nstegpal file1.bmp file2.bmp message     copies file1.bmp and the message into file2.bmp\n");
 }
 
 int main(int argc, char** argv) {
@@ -27,6 +34,7 @@ int main(int argc, char** argv) {
             break;
         default:
             fprintf(stderr, "ERROR wrong number of inputs\n");
+            help();
             exit(1);
     }
     return 0;
